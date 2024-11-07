429801ffca4cd710f0c093cf6f1c2aa0c059041e
Gabriel Castro <cg.castrorodrigues@gmail.com>
2024-11-06 21:15:50 -0300

    feat: adicionando menu de três pontos

--- LayoutApp/app/src/main/java/com/example/layoutapp/MainActivity.kt
+++ LayoutApp/app/src/main/java/com/example/layoutapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.layoutapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +16,34 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContentView(R.layout.activity_main)
+
+    WindowCompat.setDecorFitsSystemWindows(window, true)
+
+    val toolbar: Toolbar = findViewById(R.id.toolbar)
+    setSupportActionBar(toolbar)
   }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.three_dots_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.item1 -> {
+        Toast.makeText(this, "Item 1 selecionado", Toast.LENGTH_SHORT).show()
+        true
+      }
+      R.id.item2 -> {
+        Toast.makeText(this, "Item 2 selecionado", Toast.LENGTH_SHORT).show()
+        true
+      }
+      R.id.item3 -> {
+        Toast.makeText(this, "Item 3 selecionado", Toast.LENGTH_SHORT).show()
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
 }
