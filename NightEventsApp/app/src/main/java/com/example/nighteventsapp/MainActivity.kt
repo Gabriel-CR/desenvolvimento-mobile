0cf2c994f681a4554f892a751f40883c2b66aafe
Gabriel Castro <cg.castrorodrigues@gmail.com>
2024-12-12 13:50:38 -0300

    feat: adicionando detecção automática de tema

--- NightEventsApp/app/src/main/java/com/example/nighteventsapp/MainActivity.kt
+++ NightEventsApp/app/src/main/java/com/example/nighteventsapp/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.nighteventsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -40,8 +38,10 @@ class MainActivity : ComponentActivity() {
       val navController = rememberNavController()
       val drawerState = rememberDrawerState(DrawerValue.Closed)
       val scope = rememberCoroutineScope()
-      val isDarkTheme = remember { mutableStateOf(false) }
-      NightEventsAppTheme(darkTheme = isDarkTheme.value) {
+      val isDarkTheme = isSystemInDarkTheme()
+      var isDarkThemeManual by remember { mutableStateOf(isDarkTheme) }
+
+      NightEventsAppTheme(darkTheme = isDarkThemeManual) {
         ModalNavigationDrawer(
           drawerState = drawerState,
           gesturesEnabled = true,
@@ -53,7 +53,7 @@ class MainActivity : ComponentActivity() {
             Scaffold(
               topBar = {
                 TopBar(
-                  onThemeToggle = { isDarkTheme.value = !isDarkTheme.value },
+                  onThemeToggle = { isDarkThemeManual = !isDarkThemeManual },
                   onOpenDrawer = { scope.launch { drawerState.open() } }
                 )
               },
@@ -79,3 +79,4 @@ class MainActivity : ComponentActivity() {
     }
   }
 }
+
