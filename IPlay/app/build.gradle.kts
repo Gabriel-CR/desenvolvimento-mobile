864143daecaeae4aeb16aec243ce1eb7571d45d8
Gabriel Castro <cg.castrorodrigues@gmail.com>
2025-01-04 17:01:24 -0300

    infra: atualizando versão do compose

--- IPlay/app/build.gradle.kts
+++ IPlay/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 dependencies {
   implementation ("androidx.core:core-ktx:1.12.0")
   implementation ("androidx.compose.ui:ui:1.5.3")
-  implementation ("androidx.compose.material3:material3:1.x.x")
+  implementation ("androidx.compose.material3:material3:1.4.0-alpha02")
   implementation ("androidx.compose.ui:ui-tooling-preview:1.5.3")
   implementation ("androidx.navigation:navigation-compose:2.7.3")
   implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
