430f5743387414302ffd520c5b07b453ededf4be
Gabriel Castro <cg.castrorodrigues@gmail.com>
2025-01-26 17:32:32 -0300

    feat: adicionando animação no cardview

--- IPlay/app/src/main/java/com/example/iplay/ui/components/CardView.kt
+++ IPlay/app/src/main/java/com/example/iplay/ui/components/CardView.kt
@@ -1,5 +1,6 @@
 package com.example.iplay.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloatAsState
@@ -7,10 +8,12 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,6 +21,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -38,6 +42,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.iplay.models.Game
+import com.example.iplay.ui.theme.ButtonPrimaryColors
 import com.example.iplay.ui.theme.PrimaryTextColor
 import com.example.iplay.ui.theme.SecondaryTextColor
 
@@ -47,10 +52,9 @@ fun CardView(
   game: Game,
   onSetNotification: (Game) -> Unit
 ) {
-  // Estado para controlar o balanço
-  var isShaking by remember { mutableStateOf(false) }
+  var isExpanded by remember { mutableStateOf(false) }
 
-  // Animação para o balanço
+  var isShaking by remember { mutableStateOf(false) }
   val rotation by animateFloatAsState(
     targetValue = if (isShaking) 15f else 0f,
     animationSpec = infiniteRepeatable(
@@ -63,55 +67,84 @@ fun CardView(
   Card(
     modifier = Modifier
       .fillMaxWidth()
-      .clickable {
-        navController.run { navigate("gameDetails/${game.id}") }
-      },
+      .clickable { isExpanded = !isExpanded }
+      .padding(8.dp),
     shape = RoundedCornerShape(8.dp),
     elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
   ) {
-    Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
-      Image(
-        painter = painterResource(id = game.imageRes),
-        contentDescription = game.name,
-        modifier = Modifier
-          .size(64.dp)
-          .clip(CircleShape)
-      )
+    Column(
+      modifier = Modifier.padding(16.dp),
+      verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+      Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+          painter = painterResource(id = game.imageRes),
+          contentDescription = game.name,
+          modifier = Modifier
+            .size(64.dp)
+            .clip(CircleShape)
+        )
 
-      Spacer(modifier = Modifier.width(16.dp))
+        Spacer(modifier = Modifier.width(16.dp))
 
-      Column(modifier = Modifier.weight(1f)) {
-        Text(
-          text = game.name,
-          style = MaterialTheme.typography.titleMedium,
-          fontWeight = FontWeight.Bold,
-          color = PrimaryTextColor
-        )
-        Text(
-          text = game.sport,
-          style = MaterialTheme.typography.bodyMedium,
-          color = PrimaryTextColor
-        )
-        Text(
-          text = "${game.date} - ${game.time}",
-          style = MaterialTheme.typography.bodySmall,
-          color = SecondaryTextColor
-        )
+        Column(modifier = Modifier.weight(1f)) {
+          Text(
+            text = game.name,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+            color = PrimaryTextColor
+          )
+          Text(
+            text = game.sport,
+            style = MaterialTheme.typography.bodyMedium,
+            color = PrimaryTextColor
+          )
+        }
+
+        IconButton(
+          onClick = {
+            onSetNotification(game)
+            isShaking = true
+          }
+        ) {
+          Icon(
+            imageVector = Icons.Default.Notifications,
+            contentDescription = "Set Notification",
+            modifier = Modifier.graphicsLayer(
+              rotationZ = rotation
+            )
+          )
+        }
       }
 
-      IconButton(
-        onClick = {
-          onSetNotification(game)
-          isShaking = true // Ativa o balanço
-        }
-      ) {
-        Icon(
-          imageVector = Icons.Default.Notifications,
-          contentDescription = "Set Notification",
-          modifier = Modifier.graphicsLayer(
-            rotationZ = rotation
+      AnimatedVisibility(visible = isExpanded) {
+        Column(modifier = Modifier.padding(top = 8.dp)) {
+          Text(
+            text = "Data: ${game.date}",
+            style = MaterialTheme.typography.bodySmall,
+            color = SecondaryTextColor
           )
-        )
+
+          Text(
+            text = "Horário: ${game.time}",
+            style = MaterialTheme.typography.bodySmall,
+            color = SecondaryTextColor
+          )
+
+          Spacer(modifier = Modifier.height(16.dp))
+
+          Button(
+            onClick = { navController.navigate("gameDetails/${game.id}") },
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonPrimaryColors
+          ) {
+            Text("Mostrar Mais Detalhes")
+          }
+        }
+      }
+
+      if (isExpanded) {
+        Spacer(modifier = Modifier.height(16.dp))
       }
     }
   }
